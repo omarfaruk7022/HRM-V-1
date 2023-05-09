780def0b638f98de6cef09490fee35504f393088
omarfaruk7022 <farhanfaruk221@gmail.com>
2023-05-09 17:21:29 +0600

    project building success

--- src/Pages/Dashboard/Leave.js
+++ src/Pages/Dashboard/Leave.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Leave() {
-  return (
-    <div>This is Leave page</div>
-  )
+  return <div>This is Leave page</div>;
 }
